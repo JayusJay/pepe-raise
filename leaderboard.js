456e2fe054daa7b5fcd56c47347099da363d220f
awal99 <awalsulemana3@gmail.com>
2023-04-06 14:13:00 +0000

    fixed leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -66,11 +66,13 @@ async function rankUsersDesc() {
 // console.log("leader board:", leaderboardData)
 
 const leaderBoardTable = document.querySelector('[datasource="leaderboard-table"]');
+leaderBoardTable.style.height = '500px'
 
 function loadLeaderBoardData(data) {
     console.log('test:', data)
+    let rank = 0;
     data.forEach((entry, index) => {
-        let rank = index + 1
+        rank = index + 1
 
         //Creating elements
         let rankElement = ''
@@ -101,6 +103,7 @@ function loadLeaderBoardData(data) {
         //} else {
         addressElement.innerHTML = `${entry.user.substring(0, 4)}.....${entry.user.slice(-4)}`
         //s}
+
 
         if (rank <= 3) {
             if (rank == 1) rankElement = `<div class="table9_column-content"><img src="https://uploads-ssl.webflow.com/641c2b181f41df422637adc5/64201c51cc09c983c5c38739_Pepe%20First.png" loading="lazy" alt="" class="leaderboard_image">
@@ -146,21 +149,6 @@ function loadLeaderBoardData(data) {
             </div>
         `;
 
-        // leaderboardData.slice(96, 99).forEach((entry, index) => {
-        //     leaderBoardTable.appendChild(entryElement)
-        // })
-
-
-        // // Middle elment 
-        // let startIndex = 6
-        // let endIndex = 10
-        // if (leaderboardData.length > 100) {
-        //     startIndex = 96
-        //     endIndex = 99
-        // }
-        // leaderboardData.slice(startIndex, endIndex).forEach((entry, index) => {
-        //     leaderBoardTable.appendChild(entryElement)
-        // })
 
         leaderBoardTable.appendChild(entryElement)
     });
@@ -169,44 +157,13 @@ function loadLeaderBoardData(data) {
 
 function Run(leaderboardData) {
     //load first 3 entries
-    loadLeaderBoardData(leaderboardData.slice(0, 3))
+  
+    let startIndex = 4
+    let endIndex = leaderboardData.length - 1
 
-
-    if (leaderboardData.length > 3) {
-        //Appening extra elements to page , element with dots . 
-        const entryExtraElement = document.createElement('div');
-        entryExtraElement.classList.add('table_item', 'extra', 'leaderboard-entry');
-        entryExtraElement.setAttribute('role', 'row')
-        entryExtraElement.innerHTML = `
-            <div role="row" class="table_item extra">
-            <div role="cell" class="table9_column is-width-small extra">
-                <div class="dot-div">
-                    <div class="dot"></div>
-                    <div class="dot"></div>
-                </div>
-                <div fs-cmssort-type="date" fs-cmssort-field="IDENTIFIER" class="text-leaderboard-regular extra">.</div>
-                <div fs-cmssort-type="date" fs-cmssort-field="IDENTIFIER" class="text-leaderboard-regular extra">.</div>
-            </div>
-            </div>
-        `
-        leaderBoardTable.appendChild(entryExtraElement);
-    }
-
-    let startIndex = 6
-    let endIndex = 9
-    if (leaderboardData.length > 100) {
-        startIndex = 96
-        endIndex = 99
-    }
-
-    //Loading middle entries
+//load eveythin at once
     loadLeaderBoardData(leaderboardData.slice(startIndex, endIndex))
-
-
-    // Loading last entries
-    loadLeaderBoardData(leaderboardData.slice(100, 104))
-
-
+   
     function removeAllEntries() {
         // Get all the elements with the specific class name
         const elementsToRemove = document.querySelectorAll('.leaderboard-entry');
@@ -218,26 +175,6 @@ function Run(leaderboardData) {
     }
 
 
-
-    //Appending show more button when data is more than 120
-    if (leaderboardData.length > 120) {
-        let leaderBoardButtons = document.getElementById('leaderBoardButtons')
-        let showTop120ContributorsButton = document.createElement('div');
-        showTop120ContributorsButton.innerHTML = `<a href="#" id='showmore' class="button-2 is-icon contribution w-inline-block"><div class="button-text"><div class="text-block-copy">See Top 120 Contributooors</div></div></a>`
-        leaderBoardButtons.appendChild(showTop120ContributorsButton)
-
-
-        // Loading the rest levels when shomore levels button is clicked
-        const showmoreButton = document.getElementById('showmore');
-
-        // add a click event listener to the "Show More" button
-        showmoreButton.addEventListener("click", () => {
-            removeAllEntries()
-            loadLeaderBoardData(levelsData.slice(0, 120))
-            document.getElementById('showmore').style.display = 'none'
-        })
-
-    }
 
 }
 
