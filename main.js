2cee3118b3ef4b64471a873347daf4017e1bf6f6
JayusJay <atayure@gmail.com>
2023-04-30 14:41:46 +0000

    changed script locations

--- main.js
+++ main.js
@@ -535,12 +535,12 @@ window.onload = () => {
         // console.log('all milestones::', allMileStones)
 
         var milestoneTag = document.createElement("script");
-        milestoneTag.src = "https://roulette-static-files.s3.us-west-2.amazonaws.com/milestone.js";
+        milestoneTag.src = "https://peperaise.s3.amazonaws.com/milestone.js";
         document.getElementsByTagName("body")[0].appendChild(milestoneTag);
 
 
         var pepesLevelTag = document.createElement("script");
-        pepesLevelTag.src = "https://roulette-static-files.s3.us-west-2.amazonaws.com/pepeslevel.js"
+        pepesLevelTag.src = "https://peperaise.s3.amazonaws.com/pepeslevel.js"
         document.getElementsByTagName("body")[0].appendChild(pepesLevelTag);
         document.getElementById('total_raised_amount').innerHTML = '$' + parseFloat(parseFloat(ethers.utils.formatUnits(totalRaised, 6)).toFixed(2)).toLocaleString()
 
